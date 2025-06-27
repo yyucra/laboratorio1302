d2d741ee192796c97a505c73ad175140bb824967
yyucra <117547708+yyucra@users.noreply.github.com>
2025-06-26 21:25:28 -0500

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time Lima Peru is:  " + currentTime);
+    System.out.println("The current local time Lima Peru Sin min is:  " + currentTime);
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
